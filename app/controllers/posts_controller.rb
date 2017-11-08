50271f1a91752f132f29a3585f220f3a041c9059
Nick Paolino <github email address>
2017-11-08 11:40:52 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,11 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def body
+    post = Post.find(params[:id])
+    render plain: post.description
+  end
+
   def edit
   end
 
